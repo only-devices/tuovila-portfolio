6d413b88468edbeac8c63c6b0998d1d8da7b1e50
Eric Tuovila <36007317+only-devices@users.noreply.github.com>
2023-02-08 21:09:01 -0500

    Added API errors hopefully

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -117,6 +117,14 @@ export default function IndexPage() {
                 <button onClick={() => setEncryptedCaptures(document.getElementById('encryptEls-input').value)}>Capture & Encrypt Elements</button><br />
               </div>
           </Section>
+          <Section heading="The content below is fetched via API">
+            <div>
+              <span>broken image 1</span><br />
+              <img src="https://www.tuovila.com/fakeimage.jpg" alt="broken 1"/><br />
+              <span>broken image 2</span><br />
+              <img src="https://www.google.com/notarealimage.png" alt="broken 2"/><br />
+            </div>
+          </Section>
           <Section heading="*** DYNAMIC ELEMENT ID ALERT ***">
               <div>
                 The ID of <span id={dynId}><b>this specific string</b></span> is <i>{dynId}</i>. Use it in the above input boxes accordingly.
